c722813e329f765d31cb98ab234df860ee7dc0e9
leejunho0622 <lee49409780@gmail.com>
2024-11-13 17:21:13 +0900

    Add variable thread1, thread2

--- src/stopwatch/DoubleThread.java
+++ src/stopwatch/DoubleThread.java
@@ -7,13 +7,16 @@ public class DoubleThread {
 		return instance;
 	}
 	
-	void run() {
+	Thread thread1;
+	Thread thread2;
+	
+	public void run() {
 		Timer timer = new Timer();
-		Thread thread1 = new Thread(timer);
+		thread1 = new Thread(timer);
 		thread1.start();
 		
 		Stopwatch watch = new Stopwatch();
-		Thread thread2 = new Thread(watch);
+		thread2 = new Thread(watch);
 		thread2.start();
 	}
 }
