d2c3773a403ea527f50e031e4fa9e5b9cb5c9e7d
leejunho0622 <lee49409780@gmail.com>
2024-11-13 12:18:03 +0900

    Add stopwatch methods : stop(), hold(), reRun()

--- src/stopwatch/Stopwatch.java
+++ src/stopwatch/Stopwatch.java
@@ -1,9 +1,34 @@
 package stopwatch;
 
-public class Stopwatch {
+import java.util.Scanner;
+
+public class Stopwatch{
+	private Scanner scan = new Scanner(System.in);
 	private static Stopwatch instance = new Stopwatch();
+	
 	private Stopwatch() {}
 	public static Stopwatch getInstance() {
 		return instance;
+	}
+	
+	private void printMain() {
+		System.err.println("[q] STOP");
+		System.err.println("[h] HOLD");
+		System.err.println("[*] RERUN");
+		option(scan.nextLine().charAt(0));
+	}
+	
+	private void option(char select) {
+		if(select == 'q') {
+			stop();
+		}else if(select == 'h') {
+			hold();
+		}else if(select == '*') {
+			reRun();
+		}
+	}
+	
+	public void run() {
+		printMain();
 	}
 }
