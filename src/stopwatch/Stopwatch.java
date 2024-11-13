ac5565765f4f7a62e1a4477963a4279829cd0bac
leejunho0622 <lee49409780@gmail.com>
2024-11-13 16:59:36 +0900

    Add printEnd method

--- src/stopwatch/Stopwatch.java
+++ src/stopwatch/Stopwatch.java
@@ -2,41 +2,44 @@ package stopwatch;
 
 import java.util.Scanner;
 
-public class Stopwatch extends Thread{
+public class Stopwatch extends Timer{
 	private Scanner scan = new Scanner(System.in);
+	private DoubleThread stopwatch = DoubleThread.getInstance();
+	
+	private boolean isRun = true;
 	
 	private void printMain() {
 		System.err.println("[q] STOP");
 		System.err.println("[h] HOLD");
-		System.err.println("[*] RERUN");
-		option(scan.nextLine().charAt(0));
+		System.err.println("[a] RERUN");
 	}
 	
 	private void option(char select) {
-		if(select == 'q') {
-			timeStop();
-		}else if(select == 'h') {
-			timeHold();
-		}else if(select == 'a') {
-			timeReRun();
+		try {
+			if (select == 'q') {
+				Thread.sleep(100);
+				stopwatch.thread1.interrupt();
+				stopwatch.thread2.interrupt();
+				printEnd();
+			} else if (select == 'h') {
+
+			} else if (select == 'a') {
+
+			}
+		} catch (Exception e) {
+			isRun = false;
 		}
 	}
 	
-	private void timeStop() {
-		
-	}
-	
-	private void timeHold() {
-		
-	}
-	
-	private void timeReRun() {
+	public void printEnd() {
+		System.out.println(">> "+ endTime+ "소요됨");
 		
 	}
 	
 	public void run() {
 		printMain();
-		
-		
+		while(isRun) {
+			option(scan.nextLine().charAt(0));
+		}
 	}
 }
